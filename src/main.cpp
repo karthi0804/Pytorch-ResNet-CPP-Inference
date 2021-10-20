d44f80c1a6fec1cefcb8facc87810cefbadce987
Karthikeyan <karthi0804pro@gmail.com>
2021-10-20 10:28:46 +0800

    fix:input workers check

--- src/main.cpp
+++ src/main.cpp
@@ -17,17 +17,32 @@ std::vector<std::string> dataset()
 int main()
 {   // start time measurement
     std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
-	Inference predictor;
-  	std::vector<std::string> samples = dataset();
-  	int sample_count = samples.size();
-  	int num_workers;
-  	std::cout << "Input the num of workers: ";
-  	std::cin >> num_workers;
-  	predictor.predict(std::move(samples), num_workers);
-  	predictor.display(sample_count);
-  	// stop time measurement and print execution time
+    Inference predictor;
+    std::vector<std::string> samples = dataset();
+    int sample_count = samples.size();
+    int num_workers;
+    bool valid_num_workers;
+    do{
+	std::cout << "Input the num of workers: ";
+	std::cin >> num_workers;
+	if(num_workers > 0){
+		valid_num_workers = sample_count % num_workers == 0;
+		std::cout<<"Cannot do equal data split among the workers. please give a number such that the total samples count "<< sample_count
+		       	<<" is divisible and less than total sample count " << sample_count <<std::endl;
+	}
+	else
+	{
+		std::cout<<"Please give a non-zero positive number such that the total samples count "<< sample_count
+		       	<<" is divisible and less than total sample count " << sample_count <<std::endl;
+		valid_num_workers = false;
+	}
+    }
+    while(!valid_num_workers);
+    predictor.predict(std::move(samples), num_workers);
+    predictor.display(sample_count);
+    // stop time measurement and print execution time
     std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
-  	std::cout << "Inference took " << duration <<" milliseconds" << std::endl;
-  	return 0;
+    std::cout << "Inference took " << duration <<" milliseconds" << std::endl;
+    return 0;
 }
